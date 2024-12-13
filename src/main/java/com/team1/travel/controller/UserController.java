3052bd32b17303ecc7280deddd93e4bf1d9032a5
LJunHee <wnsgml4334@naver.com>
2024-12-13 17:17:11 +0900

    t

--- src/main/java/com/team1/travel/controller/UserController.java
+++ src/main/java/com/team1/travel/controller/UserController.java
@@ -1,9 +1,20 @@
 package com.team1.travel.controller;
 
+<<<<<<< HEAD
+=======
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+>>>>>>> parent of c151799 (12.13 로그인 구현(버그있음))
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+<<<<<<< HEAD
+=======
+import com.team1.travel.model.UserVo;
+import com.team1.travel.service.UserService;
+
+>>>>>>> parent of c151799 (12.13 로그인 구현(버그있음))
 
 @Controller
 public class UserController {
@@ -13,4 +24,24 @@ public class UserController {
 		return "user/signup";
 	}
 	
+<<<<<<< HEAD
+=======
+    @Autowired
+    private UserService userService;
+
+    // 회원가입
+    @PostMapping("/")
+    public String add(@ModelAttribute UserVo bean) {
+        userService.add(bean);  // 회원가입 처리
+        return "redirect:/";  // 홈 페이지로 리다이렉트
+    }
+
+    // 중복 이메일 체킹
+    @PostMapping("/check-email")
+    public ResponseEntity<String> checkEmail(@RequestParam String userEmail) {
+        boolean isAvailable = userService.isEmailAvailable(userEmail);  // 이메일 중복 체크
+        return isAvailable ? ResponseEntity.ok("available") : ResponseEntity.ok("exists");  // 결과 반환
+    }
+	
+>>>>>>> parent of c151799 (12.13 로그인 구현(버그있음))
 }

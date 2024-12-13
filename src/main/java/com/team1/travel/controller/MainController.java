550ca67872e659bb690a8cea7fe29f14ddcc5b9d
LJunHee <wnsgml4334@naver.com>
2024-12-13 17:06:22 +0900

    Revert "12.13 로그인 구현(버그있음)"
    This reverts commit c1517993960879c4a913a5af7d61034a49c73667.

--- src/main/java/com/team1/travel/controller/MainController.java
+++ src/main/java/com/team1/travel/controller/MainController.java
@@ -1,25 +1,15 @@
 package com.team1.travel.controller;
 
-import java.util.List;
-
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-
-import com.team1.travel.model.UserVo;
-
-import jakarta.servlet.http.HttpSession;
 
 
 @Controller
 public class MainController {
 
-    // index page
-    @GetMapping("/")
-    public String index(Model model, HttpSession session) {
-        UserVo user = (UserVo) session.getAttribute("loggedInUser"); 
-        model.addAttribute("user", user); 
-        return "index";
-    }
+	@GetMapping("/")
+	public String index() {
+		return "index";
+	}
 	
 }

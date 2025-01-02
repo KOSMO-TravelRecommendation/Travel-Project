f4e2c956d6fa532dd3933f027b3eb37d24d24cec
LJunHee <wnsgml4334@naver.com>
2025-01-02 17:58:31 +0900

    01.02 API 연결 테스트(미완)

--- src/main/java/com/team1/travel/controller/RestApiController.java
+++ src/main/java/com/team1/travel/controller/RestApiController.java
@@ -1,15 +1,30 @@
 package com.team1.travel.controller;
 
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:3000") // React 클라이언트의 URL
 public class RestApiController {
-	
-	@GetMapping("/api/data")
-	public String test() {
-		return "React Api Test!";
+
+	@PostMapping("/submitSurvey")
+	public ResponseEntity<String> submitSurvey(@RequestBody Map<String, Object> requestBody) {
+	    // 클라이언트에서 전달된 "inputs" 배열을 받아 처리
+	    List<String> surveyAnswers = (List<String>) requestBody.get("inputs");
+
+	    // 받은 데이터를 로그로 출력 (디버깅용)
+	    System.out.println("서버에서 받은 설문 응답: " + surveyAnswers);
+
+	    // 응답 생성
+	    return ResponseEntity.ok("서버에 설문 결과가 전달되었습니다.");
 	}
-	
+
+
 }
--- src/main/java/com/team1/travel/controller/SurveyController.java
+++ src/main/java/com/team1/travel/controller/SurveyController.java
@@ -1,16 +1,24 @@
 package com.team1.travel.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+
+import lombok.RequiredArgsConstructor;
 
 import java.util.List;
 
-@RestController
+@Controller
+@RequiredArgsConstructor
 public class SurveyController {
 
-    @PostMapping("/submit-survey")
-    public String submitSurvey(@RequestBody List<String> answers) {
-        System.out.println("설문 응답: " + answers);
-        
-        return "설문이 성공적으로 제출되었습니다!";
+    @GetMapping("/survey/form")
+    public String surveyForm() {
+        return "survey/surveyform";  // HTML 뷰를 반환
     }
+    
+//    @PostMapping("/survey/submitForm")
+//    public String submitSurvey(@RequestBody List<String> answers) {
+//        System.out.println("설문 응답: " + answers);
+//        return "설문이 성공적으로 제출되었습니다!";
+//    }
 }

c380eed6786c380baad4cd6d4e05bbfb1b950dbb
Min Jae KIM <55528705+AR23-KMJ@users.noreply.github.com>
2025-01-15 12:37:51 +0900

    Revert "01.15 favorites작업 모달 지도 수정"
    This reverts commit e3621330d248c52398371ddf5ece87e02fc5b185.

--- sql/favorites.sql
+++ sql/favorites.sql
@@ -1,9 +1,11 @@
-CREATE TABLE favorites (
-    id INT AUTO_INCREMENT PRIMARY KEY,  -- 즐겨찾기 아이디
-    user_no INT,                        -- 사용자 ID (users 테이블의 외래키)
-    place_name VARCHAR(255) NOT NULL,    -- 장소 이름
-    place_address VARCHAR(255) NOT NULL, -- 장소 주소
-    place_type INT NOT NULL,            -- 장소 유형 (1: 자연 관광지, 2: 문화 유적지 등)
-    place_score FLOAT NOT NULL,         -- 장소 평점
-    FOREIGN KEY (user_no) REFERENCES users(userNo) -- users 테이블의 userNo와 연결
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+CREATE TABLE `favorites` (
+    `favoriteNo` INT NOT NULL AUTO_INCREMENT,
+    `userNo` INT NOT NULL,
+    `attractionName` VARCHAR(255) NOT NULL DEFAULT '' COLLATE 'utf8mb4_0900_ai_ci',
+    `addedDate` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (`favoriteNo`) USING BTREE,
+    CONSTRAINT `fk_favorites_users` FOREIGN KEY (`userNo`) REFERENCES `users`(`userNo`) ON DELETE CASCADE ON UPDATE CASCADE
+)
+COLLATE='utf8mb4_0900_ai_ci'
+ENGINE=InnoDB
+AUTO_INCREMENT=1;
